b18eb72a9df64c7e447251cc7f3cc527bbc5a2ac
Sergey Mishin <sergei.a.mishin@gmail.com>
2020-12-24 04:32:22 +0300

    Update tdesktop 2.4.7 -> 2.5.1

--- pkgs/applications/networking/instant-messengers/telegram/tdesktop/tg_owt.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tdesktop/tg_owt.nix
@@ -1,24 +1,35 @@
-{ lib, stdenv, fetchFromGitHub, cmake, ninja, yasm
+{ lib, stdenv, fetchgit, cmake, ninja, yasm
 , pkg-config, libjpeg, openssl, libopus, ffmpeg, alsaLib, libpulseaudio
+, protobuf
 }:
 
 let
-  rev = "e8fcae73947445db3d418fb7c20b964b59e14706";
-  sha256 = "0s2dd41r71aixhvympiqfks1liv7x78y60n0i87vmyxyfx449b5h";
+  rev = "6eaebec41b34a0a0d98f02892d0cfe6bbcbc0a39";
+  sha256 = "0dbc36j09jmxvznal55hi3qrfyvj4y0ila6347nav9skcmk8fm64";
 
 in stdenv.mkDerivation {
   pname = "tg_owt";
   version = "git-${rev}";
 
-  src = fetchFromGitHub {
-    owner = "desktop-app";
-    repo = "tg_owt";
+  # src = fetchFromGitHub {
+  #   owner = "desktop-app";
+  #   repo = "tg_owt";
+  #   inherit rev sha256;
+  # };
+
+  src = fetchgit {
+    url = "https://github.com/desktop-app/tg_owt.git";
+    fetchSubmodules = true;
     inherit rev sha256;
   };
 
+  cmakeFlags = [
+    "-DBUILD_SHARED_LIBS:BOOL=OFF"
+  ];
+
   nativeBuildInputs = [ pkg-config cmake ninja yasm ];
 
-  buildInputs = [ libjpeg openssl libopus ffmpeg alsaLib libpulseaudio ];
+  buildInputs = [ libjpeg openssl libopus ffmpeg alsaLib libpulseaudio protobuf ];
 
   meta.license = lib.licenses.bsd3;
 }
