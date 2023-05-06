5724a7dbd753f29a128323d86d251e7d50986475
Sergey Mishin <sergei.a.mishin@gmail.com>
2023-05-06 02:09:05 +0200

    Adding flake.nix

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My Nix pkgs repository";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system} = rec {
+        sei-chain = pkgs.callPackage ./pkgs/sei-chain { };
+      };
+    };
+}
