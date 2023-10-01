4fb9a3cd434347cc67d8ce192fd6c82a1a965f0d
Sergey Mishin <sergei.a.mishin@gmail.com>
2023-10-01 19:39:46 +0200

    Fix reference to go modules

--- pkgs/cosmos/default.nix
+++ pkgs/cosmos/default.nix
@@ -59,7 +59,7 @@
         nativeBuildInputs = [ autoPatchelfHook ];
 
         preBuild = ''
-          addAutoPatchelfSearchPath ${base.go-modules}/
+          addAutoPatchelfSearchPath ${base.goModules}/
         '';
       });
 
