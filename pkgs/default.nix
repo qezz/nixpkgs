062eb7d4712abac55975c6133f12c5e923ada462
Sergey Mishin <sergei.a.mishin@gmail.com>
2023-10-01 19:55:34 +0200

    Rearrange packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -16,39 +16,15 @@ in rec {
       vendorSha256 = "sha256-6MDvoxuSMLvwguh/4PdS9K5Mdlr8c1fZDzP5asDYPDk=";
     };
 
-  osmosis = with pkgs;
+  juno = with pkgs;
     cosmos.build {
       inherit autoPatchelfHook buildGoModule;
 
-      srcRepo = "osmosis-labs/osmosis";
-      chain = "osmosis";
-      version = "v19.2.0";
-      meta = { name = "Osmosis"; };
-      vendorSha256 = "sha256-5r6dXIBzAgegI8U6QwxqB63eKLT8WTZML23EaDif+YE=";
-
-      disableGoWorkspace = true;
-    };
-
-  sei-chain = with pkgs;
-    cosmos.build {
-      inherit autoPatchelfHook buildGoModule;
-
-      srcRepo = "sei-protocol/sei-chain";
-      chain = "sei";
-      version = "v3.1.1";
-      meta = { name = "Sei Chain"; };
-      vendorSha256 = "sha256-tdkyV7cPWI20Z0nnR+3uKboAY5q/Lkg39VlKUr2l2Pc=";
-    };
-
-  neutron = with pkgs;
-    cosmos.build {
-      inherit autoPatchelfHook buildGoModule;
-
-      srcRepo = "neutron-org/neutron";
-      chain = "neutron";
-      version = "v1.0.4";
-      meta = { name = "Neutron Consumer Chain"; };
-      vendorSha256 = "sha256-jlzFYx09U7BkBg9LDZqfwT4aASQSbuVBl0a/WCrly8A=";
+      srcRepo = "CosmosContracts/juno";
+      chain = "juno";
+      version = "v17.0.0";
+      meta = { name = "Juno Chain"; };
+      vendorSha256 = "sha256-r3osfxZ8jfpUSfBTHrElVTiOr330svQi+E+s/eoxNRE=";
     };
 
   mars = with pkgs;
@@ -62,15 +38,28 @@ in rec {
       vendorSha256 = "sha256-tzxK228nBtBHdoVbdsf/3z96F8UymPdmnZExeZzq4PA=";
     };
 
-  juno = with pkgs;
+  neutron = with pkgs;
     cosmos.build {
       inherit autoPatchelfHook buildGoModule;
 
-      srcRepo = "CosmosContracts/juno";
-      chain = "juno";
-      version = "v17.0.0";
-      meta = { name = "Juno Chain"; };
-      vendorSha256 = "sha256-r3osfxZ8jfpUSfBTHrElVTiOr330svQi+E+s/eoxNRE=";
+      srcRepo = "neutron-org/neutron";
+      chain = "neutron";
+      version = "v1.0.4";
+      meta = { name = "Neutron Consumer Chain"; };
+      vendorSha256 = "sha256-jlzFYx09U7BkBg9LDZqfwT4aASQSbuVBl0a/WCrly8A=";
+    };
+
+  osmosis = with pkgs;
+    cosmos.build {
+      inherit autoPatchelfHook buildGoModule;
+
+      srcRepo = "osmosis-labs/osmosis";
+      chain = "osmosis";
+      version = "v19.2.0";
+      meta = { name = "Osmosis"; };
+      vendorSha256 = "sha256-5r6dXIBzAgegI8U6QwxqB63eKLT8WTZML23EaDif+YE=";
+
+      disableGoWorkspace = true;
     };
 
   persistence = with pkgs;
@@ -83,5 +72,16 @@ in rec {
       version = "v9.2.1";
       meta = { name = "Persistence Chain"; };
       vendorSha256 = "sha256-uMFdzS1ldf7tRajR1kHZHJOA0n/mmySoFVxd3PSiQHE=";
+    };
+
+  sei-chain = with pkgs;
+    cosmos.build {
+      inherit autoPatchelfHook buildGoModule;
+
+      srcRepo = "sei-protocol/sei-chain";
+      chain = "sei";
+      version = "v3.1.1";
+      meta = { name = "Sei Chain"; };
+      vendorSha256 = "sha256-tdkyV7cPWI20Z0nnR+3uKboAY5q/Lkg39VlKUr2l2Pc=";
     };
 }
